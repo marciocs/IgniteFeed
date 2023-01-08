5cba2b12d3bf9301921af4954ca1e4b2a65cc531
Marcio de Carvalho Silva <marciocarvsilva@gmail.com>
2023-01-08 07:22:26 -0300

    created avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
   return(
@@ -11,7 +12,7 @@ export function Sidebar(){
       />
     
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/marciocs.png" />
+        <Avatar src="https://github.com/marciocs.png" /> 
         <strong>Marcio de Carvalho</strong>
         <span>Front End</span>
       </div>

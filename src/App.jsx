6904e343103c54df1ec2d6d67299007b08007837
Marcio de Carvalho Silva <marciocarvsilva@gmail.com>
2023-01-10 18:08:58 -0300

    add constante

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
 import { Header } from "./components/Header"
 import { Post } from "./components/Post"
 
-import './global.css';
-
 import styles from './App.module.css';
 import { Sidebar } from "./components/Sidebar";
+
+import './global.css';
+
+const posts = [
+  {
+    id:1,
+    author:{
+      avatarUrl:'https://github.com/marciocs.png',
+      name:'Marcio de Carvalho Silva',
+      role: 'Desenvolvedor Front end'
+    },
+    content:[
+      {type:'paragraph', content:'Fala galera'},
+      {type:'paragraph', content:'Fala galera teste texto para descrição do site ignite feed'},
+      {type:'link', content:'Fala galera'},
+    ],
+    publishedAt: new Date('2023-01-01 20:00:00'),
+  },
+  {
+    id:2,
+    author:{
+      avatarUrl:'https://github.com/diego3g.png',
+      name:'Diego Fernandes',
+      role: 'Seo RocketSeat'
+    },
+    content:[
+      {type:'paragraph', content:'Fala galera'},
+      {type:'paragraph', content:'Fala galera teste texto para descrição do site ignite feed'},
+      {type:'link', content:'Fala galera'},
+    ],
+    publishedAt: new Date('2023-01-01 20:00:00'),
+  },
+  {
+    id:3,
+    author:{
+      avatarUrl:'https://github.com/maykbrito',
+      name:'Mayk Brito',
+      role: 'Educator Rocketseat'
+    },
+    content:[
+      {type:'paragraph', content:'Fala galera'},
+      {type:'paragraph', content:'Fala galera teste texto para descrição do site ignite feed'},
+      {type:'link', content:'Fala galera'},
+    ],
+    publishedAt: new Date('2023-01-01 20:00:00'),
+  }
+];
+
+
 
  export function App() {
 
